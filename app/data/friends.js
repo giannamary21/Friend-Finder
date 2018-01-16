698c145be3a1fbc9c39c49884dcad82bca9e871e
GIANNA ZANGRI <giannazangri@GIANNAs-MacBook-Air.local>
2018-01-16 12:46:09 -0500

    friend finder last push

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+{
+    name: 'Madea',
+    photo: 'https://az616578.vo.msecnd.net/files/2016/04/25/6359715464027751492027543703_madea%20article.jpg',
+    scores: [
+      '1',
+      '5',
+      '5',
+      '1',
+      '4',
+    ]
+  },
+  {
+  name: 'Nemo',
+    photo: 'hhttps://www.europe-travel-guides.com/wp-content/uploads/2017/05/finding-nemo-characters-with-pictures-the-best-finding-nemo-characters-images.png',
+    scores: [
+      '4',
+      '5',
+      '5',
+      '2',
+      '4',
+    ]
+},
+{
+  name: 'CatDog',
+    photo: 'https://images.genius.com/25247072b28ced1a13d935dc1445b9dd.1000x842x1.jpg',
+    scores: [
+      '1',
+      '5',
+      '2',
+      '2',
+      '1',
+    ]
+}
+]
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var PORT =  process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+			require("./app/routing/apiRoutes")(app);
+
+			require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log('Friend-Finder App is connected to PORT: ' + PORT);
+});
